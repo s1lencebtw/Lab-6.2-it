6aa685ce2a34104941dbd67abad87cf29bb9e3fb
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-05 22:56:47 +0200

    Add project files.

---
+++ Lab 6.2 it/Lab 6.2 it.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cmath>
+#include <random>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+void printArray(int* arr, int size);
+void generateArray(int* arr, int size, int min, int max);
+int findSmallestIndex(const int* arr, int size, int SmallestIndex, int SmallestNum);
+int findBiggestIndex(const int* arr, int size, int BiggestIndex, int BiggestNum);
+
+int main() {
+	const int arrSize = 10;
+	int a[arrSize];
+	generateArray(a, arrSize, -1000, 1000);
+
+	cout << "originalArray:        ";
+	printArray(a, arrSize);
+
+	int sm = findSmallestIndex(a, 10, -1, INT_MAX);
+	cout << "Index of Smallest number in Array: " << sm << endl;
+
+	int sm1 = findBiggestIndex(a, 10, -1, INT_MIN);
+	cout << "Index of Biggest number in Array:  " << sm1;
+
+}
+
+void generateArray(int* arr, int size, int min, int max) {
+	srand(unsigned(time(NULL)));
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = min + rand() % (max - min + 1);
+	}
+}
+
+void printArray(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << array[i] << " ";
+	}
+	cout << endl;
+}
+
+int findSmallestIndex(const int* arr, int size, int SmallestIndex, int SmallestNum) {
+
+	for (int i = 0; i < size; i++) {
+		if (arr[i] < SmallestNum) {
+			SmallestIndex = i;
+			SmallestNum = arr[i];
+		}
+	}
+
+	return SmallestIndex;
+}
+
+int findBiggestIndex(const int* arr, int size, int BiggestIndex, int BiggestNum) {
+
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > BiggestNum) {
+			BiggestIndex = i;
+			BiggestNum = arr[i];
+		}
+	}
+
+	return BiggestIndex;
+}
+
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.2 it/Lab 6.2 it.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[10] = { 614, -357, -781, 969, 482, 243, 693, 610, -719, -550 };
+			int c = findSmallestIndex(a, 10, -1, INT_MAX);
+			Assert::AreEqual(c, 2);
+		}
+		TEST_METHOD(TestMethod1)
+		{
+			int a[10] = { 614, -357, -781, 969, 482, 243, 693, 610, -719, -550 };
+			int t = findSmallestIndex(a, 10, -1, INT_MIN);
+			Assert::AreEqual(t, 3);
+		}
+	};
+}
